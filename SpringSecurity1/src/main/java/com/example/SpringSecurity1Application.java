a11a857a5bd2f8e15cbca42c979f2040d2762868
Mouni-Java <mounikareddykomma587@gmail.com>
2022-09-01 21:15:20 +0530

    done

---
+++ SpringSecurity1/src/main/java/com/example/SpringSecurity1Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example")
+public class SpringSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity1Application.class, args);
+	}
+
+}
